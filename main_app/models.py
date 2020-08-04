b49c741c16002d428c8cdc7a3d98a0d4256becf5
KailaMichelle <kaila22@hotmail.com>
2020-08-04 16:59:46 -0700

    Created Sign Up and city views

--- main_app/models.py
+++ main_app/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your Models here.
 
 # Profile model
 class Profile(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	city = models.CharField(max_length=100)
 	date = models.DateField('join date')
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login as auth_login
 
 # Create your views here.
 
 
 def home(request):
-	return HttpResponse('<h1>Wayfarer Home Page</h1>')
+	return render(request, 'home.html')
 
-def about(request):
-	return render(request, 'about.html')
+# User Routes
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      auth_login(request, user)
+      return redirect('home')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
+
+
+# def login(request):
+# 	return render(request, 'registration/login.html')
+
+# City Route
+def cities(request):
+	return render(request, 'cities.html')
